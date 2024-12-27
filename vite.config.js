70d9e9f3f70a858759fa936e8734f6ee67c86b89
ojan <fauzan.finances@gmail.com>
2024-12-27 12:13:14 +0700

    revise vite

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,15 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/sass/app.scss', // Your main Sass file
+                'resources/js/app.js',      // Your main JavaScript file
             ],
-            refresh: true,
+            refresh: true, // Enables hot module replacement
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Listen on all interfaces
+        port: 3000,      // Set the port to 3000
+        strictPort: true, // Exit if the port is already in use
+    },
 });
